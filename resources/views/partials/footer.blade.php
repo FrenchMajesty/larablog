3c0830099f8fd95e483106dd680e98e474635c57
FrenchMajesty <verdi.kap@gmail.com>
2018-01-15 09:23:23 -0600

    Correct invalid link to Victron in copyright
    I also added the outer class because the link was blue which is ugly.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="container-fluid footer">
 	<div class="container text-center">
 		<div class="footer-logo"><img src="{{asset('public/img/logo-black.png')}}" alt=""></div>
-		<p class="laread-motto">Designed by <a href="victron-tech.com" target="_blank">Victron</a>.</p>
+		<p class="laread-motto">Designed by <a class="outer" href="http://victron-tech.com" target="_blank">Victron</a>.</p>
 		<div class="laread-social">
 			<a href="#" class="fa fa-twitter"></a>
 			<a href="#" class="fa fa-facebook"></a>
