5484ab8c3c687cf2e6feb570d8866336221ee459
Verdi.K <Verdi@VerdiKs-MacBook-Air-2.local>
2018-01-20 11:31:55 -0600

    Add pagination to blog post manager
    I also hid the soundcloud row because it would be confusing to the user.

--- resources/views/panel/post.blade.php
+++ resources/views/panel/post.blade.php
@@ -111,40 +111,58 @@
                             </div>
                         @endif
 
-                        <div class="row post-medium nopacity animated fadeInUp">
-                            <div class="col-md-5">
-                                <div class="row" style="height: 365.625px;">
-                                    <div class="embed-responsive embed-responsive-4by3">
-                                        <iframe class="embed-responsive-item" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/186983384&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-7">
-                                <div class="post-item" style="height: 365.625px;">
-                                    <div class="medium-post-box clearfix">
-                                        <div class="pm-top-info clearfix">
-                                            <div class="pull-left">
-                                                <a href="#">SOUNCLOUD</a>
-                                            </div>
-                                            <div class="post-item-social">
-                                                <a href="#" class="quick-read"><i class="fa fa-eye"></i></a>
-                                                <a href="#" tabindex="0" role="button" data-toggle="popover" data-trigger="focus" data-placement="bottom" data-content="<a href='#'><i class='fa fa-facebook'></i></a><a href='#'><i class='fa fa-twitter'></i></a>" class="pis-share" data-original-title="" title=""><i class="fa fa-share-alt"></i></a>
-                                                <a href="#" class="post-like"><i class="fa fa-heart"></i><span>28</span></a>
-                                            </div>
-                                        </div>
-                                        <div class="post-item-paragraph">
-                                            <h2><a href="#">Sam Feldt - Show Me Love (Out Now)</a></h2>
-                                            <p class="ellipsis-readmore" style="word-wrap: break-word;">Consectetur adipiscing elit. Vivamus nec mauris pulvinar leo dignissim sollicitudin eleifend eget velit. Nunc sed dolor enim, vitaeodale diam. Mauris fermentum fringilla lorem, in rutrum massa sodales et... <a class="more" href="#">»</a></p>
-                                        </div>
-                                        <div class="pm-bottom-info clearfix">
-                                            <div class="pull-left">
-                                                21 June&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;<a href="#">#music</a>
-                                            </div>
-                                        </div>
-                                    </div>
+                        @if($posts->count() > 0)
+                        <div class="row default-post-nav">
+                            <div class="container">
+                                <div class="col-md-10 col-md-offset-2">
+                                    @if($posts->previousPageUrl())
+                                    <a href="{{$posts->previousPageUrl()}}" class="post-nav post-newer">← NEWER</a>
+                                    @endif
+
+                                    @if($posts->hasMorePages())
+                                        <a href="{{$posts->nextPageUrl()}}" class="post-nav post-older">OLDER →</a>
+                                    @endif
                                 </div>
                             </div>
                         </div>
+                    @endif
+
+                        @if(1 == 2)
+                            <div class="row post-medium nopacity animated fadeInUp">
+                                <div class="col-md-5">
+                                    <div class="row" style="height: 365.625px;">
+                                        <div class="embed-responsive embed-responsive-4by3">
+                                            <iframe class="embed-responsive-item" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/186983384&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="post-item" style="height: 365.625px;">
+                                        <div class="medium-post-box clearfix">
+                                            <div class="pm-top-info clearfix">
+                                                <div class="pull-left">
+                                                    <a href="#">SOUNCLOUD</a>
+                                                </div>
+                                                <div class="post-item-social">
+                                                    <a href="#" class="quick-read"><i class="fa fa-eye"></i></a>
+                                                    <a href="#" tabindex="0" role="button" data-toggle="popover" data-trigger="focus" data-placement="bottom" data-content="<a href='#'><i class='fa fa-facebook'></i></a><a href='#'><i class='fa fa-twitter'></i></a>" class="pis-share" data-original-title="" title=""><i class="fa fa-share-alt"></i></a>
+                                                    <a href="#" class="post-like"><i class="fa fa-heart"></i><span>28</span></a>
+                                                </div>
+                                            </div>
+                                            <div class="post-item-paragraph">
+                                                <h2><a href="#">Sam Feldt - Show Me Love (Out Now)</a></h2>
+                                                <p class="ellipsis-readmore" style="word-wrap: break-word;">Consectetur adipiscing elit. Vivamus nec mauris pulvinar leo dignissim sollicitudin eleifend eget velit. Nunc sed dolor enim, vitaeodale diam. Mauris fermentum fringilla lorem, in rutrum massa sodales et... <a class="more" href="#">»</a></p>
+                                            </div>
+                                            <div class="pm-bottom-info clearfix">
+                                                <div class="pull-left">
+                                                    21 June&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;<a href="#">#music</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
